fee68ff434f35ba597befad818b8210a5509438c
Karvein <bkzwei201@gmail.com>
2022-04-01 17:52:38 +0800

    change structure

---
+++ demo01/src/karvein/test06/CountSort.java
@@ -0,0 +1,54 @@
+package karvein.test06;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class CountSort {
+
+    public final static int[] intArray = {25,68,69,4,858,269,5,652,656,15262,25874,51,11,15,26,8064,33,1,9,0};
+
+    public int[] countSort(int[] sourceArray){
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int maxValue = getMaxValue(arr);
+
+        return countingSort(arr, maxValue);
+    }
+
+    private int[] countingSort(int[] arr, int maxValue) {
+        int bucketLen = maxValue + 1;
+        int[] bucket = new int[bucketLen];
+
+        for (int value : arr) {
+            // 一个桶里可能有多个相同的值
+            bucket[value]++;
+        }
+
+        int sortedIndex = 0;
+        for (int j = 0; j < bucketLen; j++) {
+            while (bucket[j] > 0) {
+                arr[sortedIndex++] = j;
+                bucket[j]--;
+            }
+        }
+        return arr;
+    }
+
+    private int getMaxValue(int[] arr) {
+        int maxValue = arr[0];
+        for (int value : arr) {
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
+
+    @Test
+    public void test() {
+        PrintArray.print(countSort(intArray));
+    }
+
+}
--- demo01/src/karvein/test07/test.java
+++ demo01/src/karvein/test07/test.java
@@ -1,8 +1,7 @@
-package karvein.tset07;
+package karvein.test07;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Queue;
 
 public class test {
     public static void main(String[] args) {
